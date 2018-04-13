34be65ac56bc83802dec8b8f12f451009d5d1f4c
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-04-13 10:14:03 -0500

    Update for rebase

--- invictrix.mk
+++ invictrix.mk
@@ -2,8 +2,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, vendor/invictrix/config/common.mk)
-$(call inherit-product, vendor/invictrix/config/gsm.mk)
+$(call inherit-product, vendor/invictrix/config/common_full_phone.mk)
 $(call inherit-product, vendor/invictrix/config/caf_fw.mk)
 
 # Vendor blobs
