5f686994674afb32d45ac8f6e56debf8926d1ef5
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-05-06 22:56:44 -0500

    Add device-specific settings

--- device.mk
+++ device.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/essential/mata/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
+# Essential Settings
+PRODUCT_PACKAGES += \
+    EssentialSettings
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
